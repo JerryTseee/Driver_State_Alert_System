1ad320196e0774bde1424c34670e146e0be9a2e9
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-03-29 16:40:18 +0800

    Add files via upload

--- FaceEyeDetection_Video.py
+++ FaceEyeDetection_Video.py
@@ -8,13 +8,20 @@ eye_detector = cv2.CascadeClassifier("C:\\opencv\\build\etc\\haarcascades\\haarc
 #open video capture
 video_capture = cv2.VideoCapture("C:\\Users\\admin\\Videos\\Jerry.mp4")
 
+#initialize the shortest distance of two points, set the beggining value as the infinity
+shortest_distance = float("inf")
+
 #processing inside this while loop
 while True:
 
     #read a clip from the video
     ret, original = video_capture.read()
 
-    #break the loop if no frame is retrieved
+    # get the middle point of the whole video
+    original_width, original_height, _ = original.shape
+    midPoint = (original_height//2, original_width//2)
+
+    #break the loop if no clip is retrieved
     if not ret:
         break
 
@@ -27,19 +34,32 @@ while True:
     #results lists coordinates (x,y,w,h) of bounding boxes around the detected object
     #perform face detection
     results = face_detector.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5, minSize=(30,30), flags=cv2.CASCADE_SCALE_IMAGE)
-    for (x,y,w,h) in results:
+    for (x1,y1,w1,h1) in results:
         #draws a green rectangle around each detected face and creates regions of interest(ROI) 
-        img = cv2.rectangle(original,(x,y),(x+w,y+h),(0,255,0))#(x,y) are the coordinates of top-left corner of rectangle, obtained by detected face. (x+w, y+h) is the coordinates of the bottom-right corner of the rectangle from detected face
+        img = cv2.rectangle(original,(x1, y1),(x1 + w1, y1 + h1),(0,255,0))#(x,y) are the coordinates of top-left corner of rectangle, obtained by detected face. (x+w, y+h) is the coordinates of the bottom-right corner of the rectangle from detected face
+        
+        head_middle = (x1+(w1//2), y1+(h1//2))#the coordinate of middle of the head
+        cv2.circle(original, head_middle, 5, (0,0,255), -1)#then draw the middle of the head
+        cv2.line(original, head_middle, midPoint, (0,0,255), 1)#connect the line between head_middle and midPoint
+
+        #then calculate the distance between these two points
+        shortest_distance = np.linalg.norm(np.array(head_middle) - np.array(midPoint))
+        
 
     #extract regions of interest (ROI) from the grayscale image (gray_img) and the original color image (img) based on the coordinates of the detected faces.
-    roi_gray = gray_img[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
+    roi_gray = gray_img[y1 : y1 + h1, x1 : x1 + w1]
+    roi_color = img[y1 : y1 + h1, x1 : x1 + w1]
 
     #for eye detection, detects eyes within each face region and draws rectangles around them
     eyes = eye_detector.detectMultiScale(roi_gray)
-    for (x,y,w,h) in eyes:
-        cv2.rectangle(roi_color,(x,y),(x+w,y+h),(0,255,0))
+    for (x2, y2, w2, h2) in eyes:
+        cv2.rectangle(roi_color,(x2, y2),(x2 + w2, y2 + h2),(0,255,0))
 
+    text = "Head Position Index: " + str(shortest_distance)
+    cv2.putText(original,text, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
+
+    #draw the middel point of whole video:
+    cv2.circle(original, midPoint, 5, (0,0,255), -1)
 
     #show the result
     cv2.imshow('output',img)
