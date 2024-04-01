f7fc01a38adf79e6dcd3bf154ad013c6c059b145
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-04-01 18:22:13 +0800

    Add files via upload

--- FaceEyeDetection_Video.py
+++ FaceEyeDetection_Video.py
@@ -5,8 +5,8 @@ import numpy as np
 face_detector = cv2.CascadeClassifier("C:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml")
 eye_detector = cv2.CascadeClassifier("C:\\opencv\\build\etc\\haarcascades\\haarcascade_eye.xml")
 
-#open video capture
-video_capture = cv2.VideoCapture("C:\\Users\\admin\\Videos\\Jerry.mp4")
+#open video capture, if the arguement is 0, then it will open computer camera
+video_capture = cv2.VideoCapture(0)
 
 #initialize the shortest distance of two points, set the beggining value as the infinity
 shortest_distance = float("inf")
@@ -34,6 +34,7 @@ while True:
     #results lists coordinates (x,y,w,h) of bounding boxes around the detected object
     #perform face detection
     results = face_detector.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5, minSize=(30,30), flags=cv2.CASCADE_SCALE_IMAGE)
+    
     for (x1,y1,w1,h1) in results:
         #draws a green rectangle around each detected face and creates regions of interest(ROI) 
         img = cv2.rectangle(original,(x1, y1),(x1 + w1, y1 + h1),(0,255,0))#(x,y) are the coordinates of top-left corner of rectangle, obtained by detected face. (x+w, y+h) is the coordinates of the bottom-right corner of the rectangle from detected face
@@ -55,8 +56,14 @@ while True:
     for (x2, y2, w2, h2) in eyes:
         cv2.rectangle(roi_color,(x2, y2),(x2 + w2, y2 + h2),(0,255,0))
 
+        #measure the area of eye
+        eye_area = w2*h2
+
     text = "Head Position Index: " + str(shortest_distance)
     cv2.putText(original,text, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
+
+    text = "Eye Area: " + str(eye_area)
+    cv2.putText(original,text, (10,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
 
     #draw the middel point of whole video:
     cv2.circle(original, midPoint, 5, (0,0,255), -1)
