4b91e1a8576a7fa11f453aad93a32fc28c4e9399
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-04-03 21:50:25 +0800

    Add files via upload

--- checkDangers.py
+++ checkDangers.py
@@ -4,21 +4,33 @@ import findGenderAndAge
 import time
 
 
+
 #the function is used to detect the danger driving
 #when these parameters under certain range, then it is safe, or it is dangerous
-def check(original, shortest_distance, eye_area, age):
+def check(original, shortest_distance, eye_area, age, eye_close_times):
 
-    output = "Driver State: Safe Driving"
+    output = "Driver State: Safe Driving"   
 
-    #initialize the close eye numbers
-    close_eye_number = 0
-
+    #if head position is not correct -> danger
     if shortest_distance > 150:
         output = "Driver State: Danger Driving!!!"
+
+    #if you close your eye frequently -> danger
     if eye_area < 600:
+        eye_close_times += 1
+    else:
+        #once you open your eye again, it will clear to zero and restart the counting
+        eye_close_times = 0
+    if eye_close_times > 15:
         output = "Driver State: Danger Driving!!!"
+
+    #if your age is under 20 -> danger
     if int(age) < 20:
         output = "Driver State: Danger Driving!!!"
 
-    cv2.putText(original, output, (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
-    return output
+
+    #print the result
+    text = "Eye Close Times: "+str(eye_close_times)
+    cv2.putText(original, text, (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
+    cv2.putText(original, output, (10, 190), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
+    return eye_close_times
